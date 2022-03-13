e190413e9ee13471b0fa2e88f8957b0ec2b609d2
WalidAlrefai <w.alrefai.3@gmail.com>
2022-03-13 15:36:16 +0300

    test

--- __tests__/v2.test.js
+++ __tests__/v2.test.js
@@ -1,14 +1,17 @@
 'use strict';
 process.env.SECRET = 'walid' || process.env.SECRET;
 const supertest = require('supertest');
-const server  = require('../src/server').app;
+const server = require('../src/server');
 const { db } = require('../src/auth/models/index.js');
-const mockRequest = supertest(server);
+const mockRequest = supertest(server.app);
+console.log(mockRequest);
+// const request = supertest(server);
+let id;
 let users = {
-    user: { username: 'user', password: 'password', role: 'user' },
-    writer: { username: 'writer', password: 'password', role: 'writer' },
-    editor: { username: 'editor', password: 'password', role: 'editor' },
     admin: { username: 'admin', password: 'password', role: 'admin' },
+    editor: { username: 'editor', password: 'password', role: 'editor' },
+    writer: { username: 'writer', password: 'password', role: 'writer' },
+    user: { username: 'user', password: 'password', role: 'user' },
 };
 beforeAll(async () => {
     await db.sync();
@@ -18,58 +21,59 @@ afterAll(async () => {
 });
 
 describe('V2 Test', () => {
-    Object.keys(users).forEach(userRole => {
-        describe(`${userRole} users`, () => {
+    Object.keys(users).forEach(user => {
+        describe(`${user} users`, () => {
             it.skip('create new record', async () => {
-                const register = await mockRequest.post('/signup').send(users[userRole]);
+                const register = await mockRequest.post('/signup').send(users[user]);
                 const token = register.body.token;
+                // console.log("11111111111111111111",token);
                 const response = await mockRequest.post('/api/v2/img').send({
-                    "imgUrl": "www.example.com/img",
-                    "imgName": "floara"
+                    imgUrl: "test",
+                    imgName: "test"
                 }).set("Authorization", `Bearer ${token}`);
-                console.log("111111111111111",userRole);
+                // console.log("2222222222222222",response);
+                id = response.body.id
+                console.log(response.body.id);
+
                 
-                if (userRole === 'user') {
-                    expect(response.status).not.toBe(201);
-                } else {
-                    console.log('22222222',response);
-                    expect(response.status).toBe(201);
-                }
+            if (user === 'writer' || user === 'editor'||user === 'admin') {
+                expect(response.status).toBe(201);
+            } else {
+                expect(response.status).toBe(500);
+            } 
             });
             it('get all records', async () => {
-                const register = await mockRequest.post('/signin').auth(users[userRole].username, users[userRole].password);
-                const token = register.body.token;
-                await mockRequest.put('/api/v2/img').send({
-                    "imgUrl": "www.example.com/img",
-                    "imgName": "floara"
-                }).set('Authorization', `Bearer admin`);
-                const response = await mockRequest.get('/api/v2/img').set('Authorization', `Bearer ${token}`);
-                expect(response.status).toBe(200);
+                let Auth = await mockRequest.post('/signin').auth(users[user].username,users[user].password);
+                let  token = Auth.body.token;
+                const response = await mockRequest.get('/api/v2/img').set('Authorization', `Bearer ${token}`)
+                expect(response.status).toEqual(200)
             });
             it('get one record', async () => {
-                const register = await mockRequest.post('/signin').auth(users[userRole].username, users[userRole].password);
+                const register = await mockRequest.post('/signin').auth(users[user].username, users[user].password);
                 const token = register.body.token;
-                const response = await mockRequest.get('/api/v2/img/1').set('Authorization', `Bearer ${token}`);
+                const response = await mockRequest.get(`/api/v2/img/${id}`).set('Authorization', `Bearer ${token}`);
                 expect(response.status).toBe(200);
             });
             it.skip('update record', async () => {
-                const register = await mockRequest.post('/signin').auth(users[userRole].username, users[userRole].password);
+                const register = await mockRequest.post('/signin').auth(users[user].username, users[user].password);
                 const token = register.body.token;
-                const response = await mockRequest.put('/api/v2/img/1').send({
-                    "imgUrl": "www.example.com/img",
-                    "imgName": "flora"
-                }).set('Authorization', `Bearer ${token}`);
-                if (users[userRole].role === 'user' || users[userRole].role === 'writer') {
-                    expect(response.status).not.toBe(201);
-                } else {
+
+                
+                const response = await mockRequest.put(`/api/v2/img/${id}`).send({
+                    imgUrl: "test1",
+                    imgName: "test2"
+                }).set("Authorization", `Bearer ${token}`);
+                if (user == 'editor'||user == 'admin') {
                     expect(response.status).toBe(201);
+                } else {
+                    expect(response.status).toBe(500);
                 }
             });
             if ('delete record', async () => {
-                const register = await mockRequest.post('/signin').auth(users[userRole].username, users[userRole].password);
+                const register = await mockRequest.post('/signin').auth(users[user].username, users[user].password);
                 const token = register.body.token;
                 const response = await mockRequest.delete('/api/v2/img/1').set('Authorization', `Bearer ${token}`);
-                if (users[userRole].role === 'admin') {
+                if (users[user].role === 'admin') {
                     expect(response.status).toBe(204);
                 } else {
                     expect(response.status).not.toBe(204);
@@ -78,3 +82,76 @@ describe('V2 Test', () => {
         });
     });
 });
+
+
+// let Users = {
+//     admin: { username: 'admin', password: 'password', role: 'admin' },
+//     editor: { username: 'editor', password: 'password', role: 'editor' },
+//     writer: { username: 'writer', password: 'password', role: 'writer' },
+//     user: { username: 'user', password: 'password', role: 'user' },
+// };
+// beforeAll(async () => {
+//     await db.sync();
+// })
+// afterAll(async () => {
+//     await db.drop();
+// })
+// Object.keys(Users).forEach(element => {
+//     describe('testing sport model for v2 route', () => {
+
+//         it('post new sport', async () => {
+//             let Auth = await request.post('/signup').send(Users[element]);
+//             let userToken = Auth.body.token;
+//             const response = await request.post('/api/v2/img').send({
+//                 imgUrl: "test",
+//                 imgName: "test"
+//             }).set("Authorization", `Bearer ${userToken}`);
+//             id = response.body.id
+//             if (element === 'writer' || element === 'editor' || element === 'admin') {
+//                 expect(response.status).toBe(201);
+//             } else {
+//                 expect(response.status).toBe(500);
+//             }
+//         });
+//         it('testing get all sport', async () => {
+//             let Auth = await request.post('/signin').auth(Users[element].username, Users[element].password);
+//             let userToken = Auth.body.token;
+//             const response = await request.get('/api/v2/img').set('Authorization', `Bearer ${userToken}`)
+//             expect(response.status).toEqual(200)
+//         })
+//         it('testing get one sport by id', async () => {
+//             let Auth = await request.post('/signin').auth(Users[element].username, Users[element].password);
+//             let userToken = Auth.body.token;
+//             const response = await request.get(`/api/v2/img/${id}`).set('Authorization', `Bearer ${userToken}`)
+//             expect(response.status).toEqual(200)
+//         })
+
+
+//         it('update new sport', async () => {
+//             let Auth = await request.post('/signin').auth(Users[element].username, Users[element].password);
+//             let userToken = Auth.body.token;
+//             const response = await request.put(`/api/v2/img/${id}`).send({
+//                 imgUrl: "test1",
+//                 imgName: "test2"
+//             }).set("Authorization", `Bearer ${userToken}`);
+//             if (element == 'editor' || element == 'admin') {
+//                 expect(response.status).toBe(201);
+//             } else {
+//                 expect(response.status).toBe(500);
+//             }
+//         });
+
+//         it('deleting sport by id', async () => {
+//             let Auth = await request.post('/signin').auth(Users[element].username, Users[element].password);
+//             let userToken = Auth.body.token;
+//             const response = await request.delete(`/api/v2/img/${id}`).set("Authorization", `Bearer ${userToken}`);
+
+//             if (Users[element].role === 'admin') {
+//                 expect(response.status).toBe(204);
+//             } else {
+//                 expect(response.status).toBe(500);
+//             }
+//         })
+
+//     })
+// });
